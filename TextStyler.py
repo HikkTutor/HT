ae5350e8d3cdaba23a93ce785734558fb543d638
Deformanov <deformanov@gmail.com>
2026-03-29 17:55:11 +0500

    Updating emoji instructions and logic

--- TextStyler.py
+++ TextStyler.py
@@ -1,4 +1,4 @@
-__version__ = (1, 0, 5)
+__version__ = (1, 0, 6)
 #ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ© Copyright 2024
 #ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤhttps://t.me/unnic
 # 🔒ㅤㅤㅤㅤㅤLicensed under the GNU AGPLv3
@@ -23,7 +23,7 @@ __version__ = (1, 0, 5)
 #░╚═════╝░╚═╝░░╚══╝╚═╝░░╚══╝╚═╝░╚════╝
 from telethon import events
 from telethon.tl.types import Message
-from .. import loader
+from .. import loader # type: ignore
 
 @loader.tds
 class TextStylerMod(loader.Module):
@@ -45,49 +45,49 @@ class TextStylerMod(loader.Module):
             loader.ConfigValue(
                 "enable_bold",
                 False,
-                lambda: "Включить/выключить автоматическое жирное форматирование текста.",
+                lambda: "Вкл/выкл автоматическое жирное форматирование текста.",
                 validator=loader.validators.Boolean()
             ),
             loader.ConfigValue(
                 "enable_italic",
                 False,
-                lambda: "Включить/выключить автоматическое курсивное форматирование текста.",
+                lambda: "Вкл/выкл автоматическое курсивное форматирование текста.",
                 validator=loader.validators.Boolean()
             ),
             loader.ConfigValue(
                 "enable_strikethrough",
                 False,
-                lambda: "Включить/выключить автозачеркнутый текст.",
+                lambda: "Вкл/выкл автозачеркивание текста.",
                 validator=loader.validators.Boolean()
             ),
             loader.ConfigValue(
                 "enable_underlined",
                 False,
-                lambda: "Включить/выключить автоматическое подчеркивание текста.",
+                lambda: "Вкл/выкл автоматическое подчеркивание текста.",
                 validator=loader.validators.Boolean()
             ),
             loader.ConfigValue(
                 "enable_spoiler",
                 False,
-                lambda: "Включить/выключить автоматический скрытый текст.",
+                lambda: "Вкл/выкл автоматический скрытый текст.",
                 validator=loader.validators.Boolean()
             ),
             loader.ConfigValue(
                 "enable_mono",
                 False,
-                lambda: "Включить/выключить автоматический моноширинный текст.",
+                lambda: "Вкл/выкл автоматический моноширинный текст.",
                 validator=loader.validators.Boolean()
             ),
             loader.ConfigValue(
                 "enable_quoted",
                 False,
-                lambda: "Включить/выключить автоматический цитируемый текст.",
+                lambda: "Вкл/выкл автоматический цитируемый текст.",
                 validator=loader.validators.Boolean()
             ),
             loader.ConfigValue(
                 "enable_code_block",
                 False,
-                lambda: "Включить/выключить автоматический блок кода.",
+                lambda: "Вкл/выкл автоматический блок кода.",
                 validator=loader.validators.Boolean()
             ),
             loader.ConfigValue(
@@ -101,6 +101,33 @@ class TextStylerMod(loader.Module):
     async def client_ready(self, client, db):
         self._client = client
         client.add_event_handler(self.message_handler, events.NewMessage())
+
+    async def is_premium(self, user_id: int) -> bool:
+        try:
+            user = await self._client.get_entity(user_id)
+            if hasattr(user, 'premium') and user.premium:
+                return True
+            return False
+        except Exception:
+            return False
+        
+    def get_emojis(self, is_premium: bool):
+        if is_premium:
+            return {
+                "common0": "<tg-emoji emoji-id=5875462364110787088>🗂</tg-emoji>",
+                "common1": "<tg-emoji emoji-id=5807557921843715576>⬇️</tg-emoji>",
+                "common2": "<tg-emoji emoji-id=5778335621491723621>📷</tg-emoji>",
+                "common3": "<tg-emoji emoji-id=5988023995125993550>🛠</tg-emoji>",
+                "common4": "<tg-emoji emoji-id=5879770735999717115>👤</tg-emoji>"
+            }
+        else:
+            return {
+                "common0": "📋",
+                "common1": "⬇️",
+                "common2": "◽️",
+                "common3": "🛠",
+                "common4": "👤"
+            }
 
     def format_text(self, text):
         if self.config["enable_bold"]:
@@ -146,19 +173,24 @@ class TextStylerMod(loader.Module):
             print(f"Error in message_handler: {e}")
   
     async def tshelpcmd(self, message: Message):
-        """Показывает инструкцию по использованию модуля"""
+        """Инструкция по модулю"""
+        is_premium_user = await self.is_premium(message.sender_id)
+        emojis = self.get_emojis(is_premium_user)
+        
         instruction = (
-            "<blockquote><b>Информация о модуле и его функционале</b></blockquote>\n"
-            "<blockquote><b><emoji document_id=4971987363145188045>🛑</emoji> Все исходящие сообщения автоматически форматируются, если включено то или инное форматирование.</b>\n"
-            "<emoji document_id=4971987363145188045>🛑</emoji> <b>Если сообщение начинается с символа игнорирования, оно не будет изменено.</b>\n"
-            "<emoji document_id=4971987363145188045>🛑</emoji> <b>Модуль не изменяет текст, содержащий только эмодзи, стикеры, GIF и медиафайлы.</b>\n"
-            "<emoji document_id=4971987363145188045>🛑</emoji> <b>Модуль не действует в каналах.</b>\n"
-            "<emoji document_id=4971987363145188045>🛑</emoji> <b>Hikka не поддерживает форматирование: <code><𝗕𝗹𝗼𝗰𝗸𝗾𝘂𝗼𝘁𝗲></code></b>\n"
-            "<emoji document_id=4971987363145188045>🛑</emoji> <b>Эмодзи и стикеры в тексте останутся без изменений, если выключены все форматирования.</b>\n\n"
-            "<emoji document_id=4971987363145188045>🛑</emoji> <b>Информация настроек:</b>\n"
-            "<emoji document_id=4971987363145188045>🛑</emoji> <b>Символ игнорирования по умолчанию: «<code>.</code>»</b>\n"
-            "<emoji document_id=4971987363145188045>🛑</emoji> <b>Ты можешь изменить игнорируемый символ на любой другой 1 символ, через конфиг.</b>\n"
-            "<emoji document_id=4971987363145188045>🛑</emoji> <b>Ты можешь включить/выключить игнор для каналов.</b></blockquote>\n"
-            "<blockquote><emoji document_id=5875452644599795072>🔞</emoji> <b>Разработчик: @unnic</b></blockquote>"
+            f"<blockquote><b>{emojis['common0']} Справка по модулю</b> {emojis['common1']}</blockquote>\n"
+            f"<blockquote><b>{emojis['common2']} Все исходящие сообщения автоматически форматируются, если включен тот или инной формат.</b>\n\n"
+            f"{emojis['common2']} Если сообщение начинается с символа игнорирования, сообщение <b>не будет изменено.</b>\n\n"
+            f"{emojis['common2']} Модуль не изменяет текст, содержащий только эмодзи, стикеры, GIF и медиафайлы.\n\n"
+            f"{emojis['common2']} Модуль не действует в каналах, если <b>включить игнор</b>, ниже настройка.\n\n"
+            f"{emojis['common2']} Hikka не поддерживает форматирование: <code>«𝗕𝗹𝗼𝗰𝗸𝗾𝘂𝗼𝘁𝗲»</code>\n\n"
+            f"{emojis['common2']} Эмодзи и стикеры в тексте останутся без изменений, <b>если выключены все форматы.</b></blockquote>\n\n"
+            f"<blockquote>{emojis['common0']} <b>Информация и настроека:</b> {emojis['common1']}</blockquote>\n"
+            f"<blockquote>{emojis['common2']} Символ игнорирования по умолчанию: «<code>.</code>».\n"
+            f"{emojis['common3']} <b>Настройка:</b> <code>.cfg TextStyler ignore_char</code>\n\n"
+            f"{emojis['common2']} Ты можешь включить/выключить <b>игнор для каналов.</b>\n"
+            f"{emojis['common3']} <b>Настройка:</b> <code>.cfg TextStyler ignore_formatting</code>\n\n"
+            f"{emojis['common2']} <b>Для новчиков:</b> Флаг «True» - <b>вкл</b>, флаг «False» - <b>выкл.</b>.</blockquote>\n\n"
+            f"<blockquote>{emojis['common4']} <b>Разработчик: @unnic</b></blockquote>"
         )
         await message.edit(instruction, parse_mode='html')
